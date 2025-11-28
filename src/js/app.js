e60903821b91f46d6dbc2ae039c223d9912e6d53
Uskov Aleksandr <alex_us_01@mail.ru>
2025-11-28 19:20:20 +0300

    4 commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,62 @@
 // TODO: write code here
+// Game field
+const gameField = document.getElementById('gameField');
+const fieldSize = 4;
 
-// comment this to pass build
-const unusedVariable = "variable";
-
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
+// Grid
+for (let i = 0; i < fieldSize * fieldSize; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    gameField.appendChild(cell);
 }
 
-console.log("app.js included");
+// Character
+const character = document.createElement('img');
+character.src = './img/goblin.png';
+
+// Random number generator
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Random position
+function placeCharacter() {
+    const randomCell = getRandomInt(0, fieldSize * fieldSize);
+    const cells = document.getElementsByClassName('cell');
+    
+    // Clear previous position
+    character.remove();
+    
+    // New position
+    cells[randomCell].appendChild(character);
+}
+
+// Moving a character
+function moveCharacter() {
+    const cells = document.getElementsByClassName('cell');
+    let currentPosition = -1;
+    
+    // Current position
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].contains(character)) {
+            currentPosition = i;
+            break;
+        }
+    }
+    
+    // Generate new position
+    let newPosition = currentPosition;
+    while (newPosition === currentPosition) {
+        newPosition = getRandomInt(0, fieldSize * fieldSize);
+    }
+    
+    // Move character
+    cells[currentPosition].removeChild(character);
+    cells[newPosition].appendChild(character);
+}
+
+// Character init
+placeCharacter();
+
+// Timer
+setInterval(moveCharacter, 1000);
